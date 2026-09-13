884cfeee5672226581a624c4c59756f39e39fc2b
Mohammed El-Ajou <mohammedalajou789@gmail.com>
2026-09-13 15:57:37 +0300

    fix: use absolute URL for og-image

--- med-app/src/app/layout.tsx
+++ med-app/src/app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
     siteName: 'MedUniSource',
     images: [
       {
-        url: '/og-image.png',
+        url: 'https://medunisource.com/og-image.png',
         width: 1200,
         height: 630,
         alt: 'MedUniSource',
